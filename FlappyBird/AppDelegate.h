eef4d1a7107ae7cc4b296f24e84a69e94c7c8ac1
Shirong Zheng <shirongzheng@shirongdembp.t-mobile.com>
2016-09-02 23:05:40 -0400

    Initial Commit

---
+++ FlappyBird/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlappyBird
+//
+//  Created by Shirong Zheng on 16/9/2.
+//  Copyright © 2016年 IconTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlappyBird/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlappyBird
+//
+//  Created by Shirong Zheng on 16/9/2.
+//  Copyright © 2016年 IconTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
